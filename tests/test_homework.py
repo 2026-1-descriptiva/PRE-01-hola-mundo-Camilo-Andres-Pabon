e83be0f7509451559b4dcf2e7be7b8a2777078ed
Camilo-Andres-Pabon <cpabonv@unal.edu.co>
2026-02-20 19:25:56 -0500

    Taller_1_Camilo Pabon
    Taller_1_Camilo Pabon

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -2,6 +2,12 @@
 
 from homework import pregunta_01, pregunta_02
 
-def pregunta_01():
-    """Return 'Hola mundo cruel!'"""
-    return "Hola mundo cruel!"
+
+def test_01():
+    """Test 01"""
+    assert pregunta_01.pregunta_01() == "Hola mundo cruel!"
+
+
+def test_02():
+    """Test 02"""
+    assert pregunta_02.pregunta_02() == "Hello cruel world!"
